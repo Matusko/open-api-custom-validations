95526204ed575f6b0bdc911209d4f46e07a38c33
matus <bartko.mat@gmail.com>
2020-12-31 00:06:53 +0100

    init commit

---
+++ src/main/java/sk/matusko/tutorial/openapicustomvalidations/OpenapicustomvalidationsApplication.java
@@ -0,0 +1,13 @@
+package sk.matusko.tutorial.openapicustomvalidations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapicustomvalidationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenapicustomvalidationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/matusko/tutorial/openapicustomvalidations/OpenapicustomvalidationsApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.matusko.tutorial.openapicustomvalidations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapicustomvalidationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
